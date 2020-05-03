d91b6de55ac7a6a4bbc879520a7c35bd6c4c8f04
Josiah Roa <josiahroa18@gmail.com>
2020-05-02 19:26:34 -0600

    Saving work

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,7 +1,16 @@
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile');
-const knex = require('knex');
-const environmentConfig = config[environment];
-const connection = knex(environmentConfig);
+// const environment = process.env.NODE_ENV || 'development';
+// const config = require('../knexfile');
+// const knex = require('knex');
+// const environmentConfig = config[environment];
+// const connection = knex(environmentConfig);
 
-module.exports = connection;
+// module.exports = connection;
+
+const knex = require('knex');
+const knexFile = require('../knexfile');
+
+// Add 'production' to DB_ENV in config vars on heroku
+// Run migrations in heroku console
+const environment = process.env.DB_ENV || 'development';
+
+module.exports = knex(knexFile[environment]);
--- knexfile.js
+++ knexfile.js
@@ -12,5 +12,17 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
+  },
+
+  // Include Heroku Postgres Add on in heroku
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 };
